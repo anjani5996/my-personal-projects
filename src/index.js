e1aa669ee194c010419ac247bb46bf3cf46455d8
anjani prakash <anjani.prakash@airfi.aero>
2020-10-09 17:10:38 +0530

    test14

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-// var values = { a: 1 };
+var values = { a: 1 };
 
-// function impureFunction(items) {
-//   var b =1;
-//   if (b==1) {
-// items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;
-//   }
+function impureFunction(items) {
+  var b =1;
+  if (b==1) {
+items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;items.a = items.a * b + 2;
+  }
 
 
-//   return items.a;
-// }
+  return items.a;
+}
 
-// var c = impureFunction(values);
-// // Now `values.a` is 3, the impure function modifies it.
+var c = impureFunction(values);
+// Now `values.a` is 3, the impure function modifies it.
 
-// var values2 ={ a: 1 };
+var values2 ={ a: 1 };
 
-// function pureFunction(a) {
-//   var b =1;
+function pureFunction(a) {
+  var b =1;
 
-//   a *= b + 2;
+  a *= b + 2;
 
-//   return a;
-// }
+  return a;
+}
 
-// var d = pureFunction(values.a);
-// // `values.a` has not been modified, it's still 1
+var d = pureFunction(values.a);
+// `values.a` has not been modified, it's still 1
